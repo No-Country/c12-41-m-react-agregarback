2536a4a741cb724e4654b9dc015f10f27384d040
lfernanda2504 <luizafernandagarcia@gmail.com>
2023-07-04 10:26:24 -0500

    desing homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Form from './components/Form/Form'
+import Home from './components/HomePage/Home'
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Home   />
+    </>
 
   )
 }
