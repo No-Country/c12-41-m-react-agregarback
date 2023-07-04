d165aa4e1cd0a46dc73b54d67e6693d2ebd892d4
aronresni <aronresni@gmail.com>
2023-07-04 16:38:55 -0300

    Upload y pruena del Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,86 +1,236 @@
-import React from "react"
+import React from "react";
+import axios from "axios";
+import validation from "./validate";
 
-import validation from "./validate"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-
-function Form(props) {
-    const [userData, setUserData] = React.useState({
+function Form() {
+    const [loginData, setLoginData] = React.useState({
         dni: "",
-        usuario: "",
+        username: "",
         clave: ""
-    })
+    });
 
-    const [errors, setErrors] = React.useState({
-    })
+    const [userRegister, setUserRegisterData] = React.useState({
+        name: "",
+        email: "",
+        password: "",
+        date_of_birth: "",
+        dni: "",
+        address: "",
+        username: "",
+        phone_number: ""
+    });
+
+    const [errors, setErrors] = React.useState({});
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setUserData({
-            ...userData,
+        setLoginData({
+            ...loginData,
             [name]: value
-        })
+        });
         setErrors(
             validation({
-                ...userData,
+                ...loginData,
                 [name]: value
             })
-        )
+        );
     };
 
-    const handleSubmit = (e) => {
+    const handleRegisterInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserRegisterData({
+            ...userRegister,
+            [name]: value
+        });
+        setErrors(
+            validation({
+                ...userRegister,
+                [name]: value
+            })
+        );
+    };
 
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        props.login(userData)
+
+        try {
+            const response = await axios.post("localhost:3000/api/v1/users/login", loginData);
+
+            console.log(response.data);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleRegisterSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+
+            const response = await axios.post("localhost:3000/api/v1/users/signup", userRegister);
+
+
+            console.log(response.data);
+
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
-
-        <div className={style.container}>
-            <div className="login">
-                <form onSubmit={handleSubmit} className={style.form}>
-                    <div>
-                        <div className="dni">
-
-                            <label>Numero de documento</label>
-                            <input className="input"
-                                placeholder="Numero de documento"
-                                type="number"
-                                name="dni"
-                                value={userData.dni}
-                                onChange={handleInputChange}
-
-                            />
-                            <p className="error">{errors.dni}</p>
+        <div className="Form">
+            <div>
+                <div className="login">
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <div>
+                                <label>Numero de documento</label>
+                                <input
+                                    className="input"
+                                    placeholder="Numero de documento"
+                                    type="number"
+                                    name="dni"
+                                    value={loginData.dni}
+                                    onChange={handleInputChange}
+                                />
+                                <p className="error">{errors.dni}</p>
+                            </div>
+                            <div className="usuario">
+                                <label>Usuario</label>
+                                <input
+                                    className="input"
+                                    placeholder="Usuario"
+                                    type="username"
+                                    name="username"
+                                    value={loginData.username}
+                                    onChange={handleInputChange}
+                                />
+                                <p className="error">{errors.usuario}</p>
+                            </div>
+                            <div className="clave">
+                                <label>Clave</label>
+                                <input
+                                    className="input"
+                                    placeholder="Clave"
+                                    type="password"
+                                    name="clave"
+                                    value={loginData.clave}
+                                    onChange={handleInputChange}
+                                />
+                                <p className="error">{errors.clave}</p>
+                                <button type="submit" onChange={handleSubmit}>Iniciar sesion</button>
+                            </div>
                         </div>
-                        <div className="usuario">
-
-                            <label>Usuario</label>
-                            <input className="input"
-                                placeholder="Usuario"
-                                type="number"
-                                name="usuario"
-                                value={userData.usuario}
-                                onChange={handleInputChange}
+                    </form>
+                </div>
+                <div className="signup">
+                    <form onSubmit={handleRegisterSubmit}>
+                        <div>
+                            <label>Nombre Completo</label>
+                            <input
+                                className="input"
+                                placeholder="Nombre Completo"
+                                type="text"
+                                name="name"
+                                value={userRegister.name}
+                                onChange={handleRegisterInputChange}
                             />
-                            <p className="error">{errors.usuario}</p>
+                            <p className="error">{errors.name}</p>
                         </div>
-                        <div className="clave">
-
-                            <label>Clave</label>
-                            <input className="input"
-                                placeholder="Clave"
+                        <div>
+                            <label>Email</label>
+                            <input
+                                className="input"
+                                placeholder="Email"
+                                type="email"
+                                name="email"
+                                value={userRegister.email}
+                                onChange={handleRegisterInputChange}
+                            />
+                            <p className="error">{errors.email}</p>
+                        </div>
+                        <div>
+                            <label>Contraseña</label>
+                            <input
+                                className="input"
+                                placeholder="Contraseña"
                                 type="password"
-                                name="clave"
-                                value={userData.clave}
-                                onChange={handleInputChange}
-
+                                name="password"
+                                value={userRegister.password}
+                                onChange={handleRegisterInputChange}
                             />
-                            <p className="error">{errors.clave}</p>
+                            <p className="error">{errors.password}</p>
                         </div>
-                    </div>
-                </form>
+                        <div>
+                            <label>Fecha de Nacimiento</label>
+                            <DatePicker
+                                className="input"
+                                selected={userRegister.date_of_birth}
+                                onChange={(date) =>
+                                    setUserRegisterData({
+                                        ...userRegister,
+                                        date_of_birth: date
+                                    })
+                                }
+                                dateFormat="yyyy-MM-dd"
+                            />
+                        </div>
+                        <div>
+                            <label>Número de Documento</label>
+                            <input
+                                className="input"
+                                placeholder="Número de Documento"
+                                type="text"
+                                name="dni"
+                                value={userRegister.dni}
+                                onChange={handleRegisterInputChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Dirección</label>
+                            <input
+                                className="input"
+                                placeholder="Dirección"
+                                type="text"
+                                name="address"
+                                value={userRegister.address}
+                                onChange={handleRegisterInputChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Nombre de Usuario</label>
+                            <input
+                                className="input"
+                                placeholder="Nombre de Usuario"
+                                type="text"
+                                name="username"
+                                value={userRegister.username}
+                                onChange={handleRegisterInputChange}
+                            />
+                            <p className="error">{errors.username}</p>
+                        </div>
+                        <div>
+                            <label>Número de Teléfono</label>
+                            <input
+                                className="input"
+                                placeholder="Número de Teléfono"
+                                type="text"
+                                name="phone_number"
+                                value={userRegister.phone_number}
+                                onChange={handleRegisterInputChange}
+                            />
+                            <p className="error">{errors.phone_number}</p>
+                        </div>
+                        <button type="submit">Registrar</button>
+                    </form>
+                </div>
             </div>
         </div>
-    )
+    );
 }
-export default Form
+
+export default Form;
--- src/components/Form/validate.js
+++ src/components/Form/validate.js
@@ -1,20 +1,62 @@
 export default function validation(input) {
 
-    const error = {}
+    const errors = {}
+
+    //regex login
 
     const regexDNI = / [0 - 9]{ 1, 3}\.?[0 - 9]{ 3, 3 } \.?[0 - 9]{ 3, 3 } /
     const regexClave = /^\d{4}$/;
     const regexUsuario = /^[a-zA-Z0-9_]{6,20}$/;
 
+    //regex signup
+    //{
+    const regexName = /^[a-zA-ZÀ-ÿ-]+(\s[a-zA-ZÀ-ÿ-]+)*$/;
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const regexBirth = /^(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/;
+    const regexAdress = /^[a-zA-Z0-9\s\.,#-]+$/;
+    const regexPhone = /^\d{10}$/;
+
+    //{
+    //    "name":"prueba desde server",
+    //    "email":"prueb2a@prueba.com",
+    //    "password":"hola",
+    //    "status":"active",
+    //    "date_of_birth":"2023-07-15",
+    //    "dni":891357839,
+    //    "address":"Calle88",
+    //    "username":"prueba1",
+    //    "phone_number":123114125
+    //}
 
 
     if (!regexDNI.test(input.dni)) {
-        error.dni = "Debe ingresar un numero de documento valido"
+        errors.dni = "Debe ingresar un numero de documento valido"
     }
     if (!regexClave.test(input.clave)) {
-        error.clave = "Ingresá tu clave de 4 dígitos."
+        errors.clave = "Ingresá tu clave de 4 dígitos."
     }
     if (!regexUsuario.test(input.usuario)) {
-        error.usuario = "Ingresa un Usuario alfanumerico"
+        errors.usuario = "Ingresa un Usuario alfanumerico"
     };
+    if (!regexName.test(input.name)) {
+        errors.name = "Ingresa un Nombre y Apellido"
+    };
+    if (!regexEmail.test(input.email)) {
+        errors.email = "Ingrese un correo valido"
+    }
+    if (!regexPassword.test(input.password)) {
+        errors.password = "Ingrese una contraseña valida"
+    }
+    if (!regexAdress.test(input.adress)) {
+        errors.adress = "Ingrese una direccion valida";
+    }
+    if (!regexUsuario.test(input.username)) {
+        errors.username = "Ingrese un nombre de usuario valido";
+    }
+    if (!regexPhone.test(input.phone_number)) {
+        errors.phone_number = "Ingrese un numero de telefono valido";
+    }
+
+    return errors;
 }
