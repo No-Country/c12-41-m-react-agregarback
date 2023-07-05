447603b77c6b26695d6d1ddd1497630d89ee0f9b
Sergio-TG <sergiotorresguaymasi@gmail.com>
2023-07-05 11:09:51 -0300

    Update Footer
    Actualizo y creo la carpeta de Footer dentro de Components para despues darle el estilo como lo hizo Gabo M.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Form from './components/Form/Form'
 import Home from './components/HomePage/Home'
+import Footer from './components/Footer/Footer'
 function App() {
 
 
   return (
     <>
       <Form />
+      <Footer />
     </>
 
   )
