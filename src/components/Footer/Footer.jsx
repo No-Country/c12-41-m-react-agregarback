462eb38d5a18645e6b92a0e2d2cbdf902ec33e90
Sergio-TG <sergiotorresguaymasi@gmail.com>
2023-07-05 22:25:37 -0300

    Footer  - Falta Estilo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,147 +2,184 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="relative mt-16 bg-deep-purple-accent-400">
-      <svg
-        className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-deep-purple-accent-400"
-        preserveAspectRatio="none"
-        viewBox="0 0 1440 54"
-      >
-        <path
-          fill="currentColor"
-          d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
-        />
-      </svg>
-      <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
-          <div className="md:max-w-md lg:col-span-2">
-            <a
-              href="/"
-              aria-label="Go home"
-              title="AccessBank"
-              className="inline-flex items-center"
-            >
-              <svg
-                className="w-8 text-teal-accent-400"
-                viewBox="0 0 24 24"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                stroke="currentColor"
-                fill="none"
-              >
-                <rect x="3" y="1" width="7" height="12" />
-                <rect x="3" y="17" width="7" height="6" />
-                <rect x="14" y="1" width="7" height="6" />
-                <rect x="14" y="11" width="7" height="12" />
-              </svg>
-              <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
-                AccessBank
-              </span>
-            </a>
-            <div className="mt-4 lg:max-w-sm">
-              <p className="text-sm text-deep-purple-50">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium, totam rem aperiam.
-              </p>
-              <p className="mt-4 text-sm text-deep-purple-50">
-                Eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                beatae vitae dicta sunt explicabo.
-              </p>
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
-            <div>
-              <p className="font-semibold tracking-wide text-teal-accent-400">
-                Category
-              </p>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <a
-                    href="/"
-                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
-                  >
-                    Clientes
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
-                  >
-                    Defensa al Consumidor
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
-                  >
-                    Protección de Datos Personales
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
-                  >
-                    Términos y Condiciones del Sitio
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <p className="font-semibold tracking-wide text-teal-accent-400">
-                Recomendaciones de Seguridad
-              </p>
-              <ul className="mt-2 space-y-2">
-                <li>
-                  <a
-                    href="/"
-                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
-                  >
-                    Política Ambiental
-                  </a>
-                </li>
-              </ul>
-            </div>
+    <footer class="w-full text-gray-700 bg-gray-100 body-font">
+      <div class="container flex flex-col flex-wrap px-5 py-20 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+        <div class="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
+          <a class="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
+            <img class="h-12" viewBox="0 0 202 69" src="./public/logoab.svg" />
+          </a>
+          <p class="mt-2 text-sm text-gray-500">
+            Accesibilidad y cuidado de su dinero
+          </p>
+          <div class="mt-4">
+            <span class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
+              <a class="text-gray-500 cursor-pointer hover:text-gray-700">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="0"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  ></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
           </div>
         </div>
-        <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
-          <p className="text-sm text-gray-100">
-            © Copyright 2023 AccessBank Inc. All rights reserved.
-          </p>
-          <div className="flex items-center mt-4 space-x-4 sm:mt-0">
-            <a
-              href="/"
-              className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
-            >
-              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
-              </svg>
-            </a>
-            <a
-              href="/"
-              className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
-            >
-              <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
-                <circle cx="15" cy="15" r="4" />
-                <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
-              </svg>
-            </a>
-            <a
-              href="/"
-              className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
-            >
-              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
-              </svg>
-            </a>
+        <div class="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
+          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+            <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">
+              About
+            </h2>
+            <nav class="mb-10 list-none">
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Aviso Legal
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Datos Personales
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Defensa al Consumidor
+                </a>
+              </li>
+            </nav>
+          </div>
+          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+            <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">
+              Developers
+            </h2>
+            <nav class="mb-10 list-none">
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Aron Resnicof
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Emilio Rivas Ruiz
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Fabricio Reyna
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Gabriel Medina
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Gabriel Shimabuku
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Luisa García
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Sergio Torres Guaymasi
+                </a>
+              </li>
+            </nav>
+          </div>
+          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+            <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">
+              Team Leader
+            </h2>
+            <nav class="mb-10 list-none">
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Yessica Rodriguez
+                </a>
+              </li>
+            </nav>
+          </div>
+          <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+            <h2 class="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">
+              Contact0
+            </h2>
+            <nav class="mb-10 list-none">
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Enviar mail
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  Atención a Clientes
+                </a>
+              </li>
+              <li class="mt-3">
+                <a class="text-gray-500 cursor-pointer hover:text-gray-900">
+                  +123-456-7890
+                </a>
+              </li>
+            </nav>
           </div>
         </div>
       </div>
-    </div>
+      <div class="bg-gray-300">
+        <div class="container px-5 py-4 mx-auto">
+          <p class="text-sm text-gray-700 capitalize xl:text-center">
+            © 2023 All rights reserved No Country
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
