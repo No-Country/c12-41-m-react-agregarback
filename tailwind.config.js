aa1a84c484f1c7f8e9389806563f1d20de761ed6
lfernanda2504 <luizafernandagarcia@gmail.com>
2023-07-05 23:06:08 -0500

    add colors tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
+    colors:{
+      'transparent': 'transparent',
+      'dark': '#1A1A1A',
+      'white': '#ffff',
+      'gray': '#333333',
+      'orange': '#FBB23F',
+      'yellow': '#F9EA32'
+    },
     extend: {},
   },
   plugins: [],
