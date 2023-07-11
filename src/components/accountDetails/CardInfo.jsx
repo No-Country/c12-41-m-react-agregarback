88794102540783bdf8b669c92649b7cc722ab30f
emilio rivas <emiliorivasruiz@gmail.com>
2023-07-11 14:50:45 -0500

    carrusel card

--- src/components/accountDetails/CardInfo.jsx
+++ src/components/accountDetails/CardInfo.jsx
@@ -1,74 +1,171 @@
-import Card from "./Card";
-import { BsCreditCard2BackFill } from "react-icons/bs";
-import { RiShakeHandsFill, RiQuestionnaireFill } from "react-icons/ri";
-import Accesos from "./Accesos";
 import { useState } from "react";
+import { BsCreditCard2BackFill } from "react-icons/bs";
+import { RiQuestionnaireFill, RiShakeHandsFill } from "react-icons/ri";
+import Accesos from "./Accesos";
+import CarruselCard from "./CarruselCard";
 
 const CardInfo = () => {
-  const [currentCard, setCurrentCard] = useState({});
-  const cards = [
+  const [listOfCards, setListOfCards] = useState([]);
+  //   {
+  //     id: 1,
+  //     cardNumber: "8219544817579717",
+  //     cvv: 406,
+  //     creditLine: 1000,
+  //     type: "masterCard",
+  //     category: "credito",
+  //     status: "active",
+  //     accountId: 1,
+  //     userId: 1,
+  //     createdAt: "2023-07-11T01:40:41.000Z",
+  //     updatedAt: "2023-07-11T01:40:41.000Z",
+  //   },
+  //   {
+  //     id: 2,
+  //     cardNumber: "2541208289745521",
+  //     cvv: 740,
+  //     creditLine: null,
+  //     type: "masterCard",
+  //     category: "debito",
+  //     status: "active",
+  //     accountId: 2,
+  //     userId: 1,
+  //     createdAt: "2023-07-11T01:40:56.000Z",
+  //     updatedAt: "2023-07-11T01:40:56.000Z",
+  //   },
+  //   {
+  //     id: 3,
+  //     cardNumber: "8578514226283121",
+  //     cvv: 527,
+  //     creditLine: null,
+  //     type: "visa",
+  //     category: "debito",
+  //     status: "active",
+  //     accountId: 2,
+  //     userId: 1,
+  //     createdAt: "2023-07-11T01:41:05.000Z",
+  //     updatedAt: "2023-07-11T01:41:05.000Z",
+  //   },
+  //   {
+  //     id: 4,
+  //     cardNumber: "6076220195236161",
+  //     cvv: 791,
+  //     creditLine: 1000,
+  //     type: "americanExpress",
+  //     category: "credito",
+  //     status: "active",
+  //     accountId: 2,
+  //     userId: 1,
+  //     createdAt: "2023-07-11T01:41:33.000Z",
+  //     updatedAt: "2023-07-11T01:41:33.000Z",
+  //   },
+  // ];
+
+  const accounts = [
     {
       id: 1,
-      cardNumber: "8219544817579717",
-      cvv: 406,
-      creditLine: 1000,
-      type: "masterCard",
-      category: "credito",
+      amount: 700,
+      accountNumber: 675168867,
+      cbu: "5279168459487761556066",
+      cvu: "1946096101960161701193",
+      alias: "user.7273",
       status: "active",
-      accountId: 1,
+      currency: "usd",
       userId: 1,
-      createdAt: "2023-07-11T01:40:41.000Z",
-      updatedAt: "2023-07-11T01:40:41.000Z",
+      createdAt: "2023-07-06T21:40:42.000Z",
+      updatedAt: "2023-07-06T22:41:05.000Z",
+      cards: [
+        {
+          id: 1,
+          cardNumber: "8219544817579717",
+          cvv: 406,
+          creditLine: 1000,
+          type: "masterCard",
+          category: "credito",
+          status: "active",
+          accountId: 1,
+          userId: 1,
+          createdAt: "2023-07-11T01:40:41.000Z",
+          updatedAt: "2023-07-11T01:40:41.000Z",
+        },
+      ],
     },
     {
       id: 2,
-      cardNumber: "2541208289745521",
-      cvv: 740,
-      creditLine: null,
-      type: "masterCard",
-      category: "debito",
+      amount: 1230,
+      accountNumber: 145168868,
+      cbu: "2764180945997177147976",
+      cvu: "3202503944480354597734",
+      alias: "user.3409",
       status: "active",
-      accountId: 2,
+      currency: "soles",
       userId: 1,
-      createdAt: "2023-07-11T01:40:56.000Z",
-      updatedAt: "2023-07-11T01:40:56.000Z",
-    },
-    {
-      id: 3,
-      cardNumber: "8578514226283121",
-      cvv: 527,
-      creditLine: null,
-      type: "visa",
-      category: "debito",
-      status: "active",
-      accountId: 2,
-      userId: 1,
-      createdAt: "2023-07-11T01:41:05.000Z",
-      updatedAt: "2023-07-11T01:41:05.000Z",
-    },
-    {
-      id: 4,
-      cardNumber: "6076220195236161",
-      cvv: 791,
-      creditLine: 1000,
-      type: "americanExpress",
-      category: "credito",
-      status: "active",
-      accountId: 2,
-      userId: 1,
-      createdAt: "2023-07-11T01:41:33.000Z",
-      updatedAt: "2023-07-11T01:41:33.000Z",
+      createdAt: "2023-07-06T22:19:00.000Z",
+      updatedAt: "2023-07-06T22:58:38.000Z",
+      cards: [
+        {
+          id: 4,
+          cardNumber: "6076220195236161",
+          cvv: 791,
+          creditLine: 1000,
+          type: "americanExpress",
+          category: "credito",
+          status: "active",
+          accountId: 2,
+          userId: 1,
+          createdAt: "2023-07-11T01:41:33.000Z",
+          updatedAt: "2023-07-11T01:41:33.000Z",
+        },
+        {
+          id: 3,
+          cardNumber: "8578514226283121",
+          cvv: 527,
+          creditLine: null,
+          type: "visa",
+          category: "debito",
+          status: "active",
+          accountId: 2,
+          userId: 1,
+          createdAt: "2023-07-11T01:41:05.000Z",
+          updatedAt: "2023-07-11T01:41:05.000Z",
+        },
+        {
+          id: 2,
+          cardNumber: "2541208289745521",
+          cvv: 740,
+          creditLine: null,
+          type: "masterCard",
+          category: "debito",
+          status: "active",
+          accountId: 2,
+          userId: 1,
+          createdAt: "2023-07-11T01:40:56.000Z",
+          updatedAt: "2023-07-11T01:40:56.000Z",
+        },
+      ],
     },
   ];
 
-  const handleChangeCurrentCard = (e) => {
-    const card = cards.find((card) => card.id == e.target.value);
-    setCurrentCard(card);
+  const hanldeSlectAccount = (e) => {
+    const account = accounts.find((account) => account.id == e.target.value);
+    setListOfCards(account.cards);
   };
 
   return (
     <section className="pt-10">
-      <article className="flex flex-col gap-6 justify-center items-center">
+      <article className="flex flex-col gap-6 justify-center items-center relative p-10">
+        <div className="flex gap-5 absolute z-20 left-[50%] translate-x-[-100%] top-[0]">
+          <span>Accounts</span>
+          <select
+            onChange={(e) => hanldeSlectAccount(e)}
+            className="outline-none text-sm text-white rounded-md bg-transparent py-1"
+          >
+            {accounts.map((account) => (
+              <option key={account.id} className="bg-gray" value={account.id}>
+                {account.accountNumber}
+              </option>
+            ))}
+          </select>
+        </div>
         <article className="relative">
           <div className="absolute right-[-20%] top-[-40%]">
             <img src="/accounts/Ellipse 10.png" alt="elipse" loading="lazy" />
@@ -76,24 +173,8 @@ const CardInfo = () => {
           <div className="absolute left-[-15%] bottom-[-30%]">
             <img src="/accounts/Ellipse 20.png" alt="elipse" loading="lazy" />
           </div>
-          <Card
-            accountNumber={currentCard.cardNumber}
-            name={"Gabriel Shymabuku"}
-            type={currentCard.type}
-            date={currentCard.createdAt}
-            img={`/accounts/${currentCard.type}.webp`}
-          />
+          <CarruselCard listOfCards={listOfCards} />
         </article>
-        <select
-          onChange={(e) => handleChangeCurrentCard(e)}
-          className="outline-none text-sm text-white rounded-md bg-transparent py-1"
-        >
-          {cards.map((card) => (
-            <option key={card.id} className="bg-gray" value={card.id}>
-              {card.cardNumber}
-            </option>
-          ))}
-        </select>
       </article>
       <article className="grid grid-cols-[repeat(auto-fill,_minmax(100px,_1fr))] gap-3 auto-cols-fr py-10">
         <Accesos text={"Servicios"} icon={<BsCreditCard2BackFill />} />
