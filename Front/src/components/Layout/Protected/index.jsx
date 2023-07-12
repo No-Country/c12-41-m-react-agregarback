d721c0c9d508c0dea21d337dc4955891cab91707
byga <gaby.shima12@gmail.com>
2023-07-12 18:03:38 -0300

    Arreglo bug de proteccion de rutas (cuando cambio direccion ahora redirige correctamente)

--- Front/src/components/Layout/Protected/index.jsx
+++ Front/src/components/Layout/Protected/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import NavMenu from '../../NavMenu'
-import Sidenav from '../../Sidenav/Sidenav'
+// import Sidenav from '../../Sidenav/Sidenav'
 import Footer from '../../Footer/Footer'
 import SideNavLanding from '../../SideNavLanding/SideNavLanding'
 
@@ -8,7 +8,7 @@ const ProtectedLayout = () => {
     return (
         <div className='min-h-screen'>
             {/* <Sidenav /> */}
-            <SideNavLanding/>
+            <SideNavLanding />
             <div className='grid grid-cols-[auto,_1fr] bg-dark p-3 h-full gap-4 relative'>
                 <NavMenu />
                 <Outlet />
