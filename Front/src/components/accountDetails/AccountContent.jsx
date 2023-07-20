e1ccbe78291fb52737e2227ccf30ab20fbcb6b3a
lfernanda2504 <luizafernandagarcia@gmail.com>
2023-07-20 13:49:37 -0500

    bug  accountContent

--- Front/src/components/accountDetails/AccountContent.jsx
+++ Front/src/components/accountDetails/AccountContent.jsx
@@ -1,99 +1,53 @@
-import { useEffect, useState } from "react";
-import CardInfo from "./CardInfo";
-import UltimosMovimientos from "./UltimosMovimientos";
-import axios from "axios";
-import { GridLoader } from "react-spinners";
+import { useEffect, useState } from "react"
+import CardInfo from "./CardInfo"
+import UltimosMovimientos from "./UltimosMovimientos"
+import axios from 'axios'
+import { GridLoader } from "react-spinners"
 import { BsCreditCard2BackFill } from "react-icons/bs";
 import { FaHandHoldingDollar } from "react-icons/fa6";
-import {
-  AiOutlineLineChart,
-  AiFillEyeInvisible,
-  AiFillEye,
-} from "react-icons/ai";
+import { AiOutlineLineChart, AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { RiQuestionnaireFill, RiShakeHandsFill } from "react-icons/ri";
 import Accesos from "./Accesos";
-import ModalNewAccount from "./ModalNewAccount";
-import ModalNewCard from "./ModalNewCard";
+import ModalNewAccount from "./ModalNewAccount"
+import ModalNewCard from "./ModalNewCard"
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux"
 
 const AccountContent = () => {
-  const { status, data } = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user.data);
   const headers = {
-    Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-  };
-  const [showBalance, setShowBalance] = useState(false);
+    Authorization: `Bearer ${sessionStorage.getItem('token')}`
+  }
+
+  console.log(user)
+  const [showBalance, setShowBalance] = useState(false)
   const [accounts, setAccounts] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [showNewAccountModal, setShowNewAccountModal] = useState(false);
   const [showNewCardModal, setShowNewCardModal] = useState(false);
   useEffect(() => {
+
     const getAccounts = async () => {
       try {
-        const response = await axios.get(
-          `https://nocountrybackend.onrender.com/api/v1/account/${sessionStorage.getItem(
-            "userId"
-          )}`,
-          { headers }
-        );
-        setAccounts(response.data.accounts);
-        setIsLoaded(true);
+        const response = await axios.get(`https://nocountrybackend.onrender.com/api/v1/account/${sessionStorage.getItem('userId')}`, { headers })
+        setAccounts(response.data.accounts)
+        setIsLoaded(true)
       } catch (error) {
         console.log(error);
-        setIsLoaded(true);
+        setIsLoaded(true)
+
       }
-    };
+    }
     getAccounts();
-  }, []);
+
+  }, [])
 
   const handleSelectAccount = (e) => {
-    setSelectedIndex(e.target.value);
+    setSelectedIndex(e.target.value)
   };
 
-  return isLoaded ? (
-    <section className="grid lg:grid-cols-2 overflow-hidden p-3 pt-16 gap-4">
-      {accounts.length > 0 ? (
-        <>
-          <div className="">
-            <span>Cuenta en:</span>
-            <select
-              onChange={(e) => handleSelectAccount(e)}
-              className="outline-none text-sm text-white rounded-md bg-transparent py-1"
-            >
-              {accounts.map((account, index) => (
-                <option key={account.id} className="bg-gray" value={index}>
-                  {account.currency.charAt(0).toUpperCase() +
-                    account.currency.slice(1)}
-                </option>
-              ))}
-            </select>
-            <div>
-              <h2 className="text-xl">
-                Saldo: ${accounts[selectedIndex].amount}
-              </h2>
-            </div>
-            <CardInfo account={accounts[selectedIndex]} />
-            <article className="grid grid-cols-[repeat(auto-fill,_minmax(100px,_1fr))] gap-3 auto-cols-fr py-10">
-              <Accesos text={"Inversiones"} icon={<AiOutlineLineChart />} />
-              <Accesos text={"prestamos"} icon={<FaHandHoldingDollar />} />
-              <NavLink to="/preguntasfrecuentes">
-                <Accesos text={"Contactanos"} icon={<RiQuestionnaireFill />} />
-              </NavLink>
-              <Accesos
-                text={"Solicitar nueva tarjeta"}
-                icon={
-                  <BsCreditCard2BackFill
-                    onClick={() => setShowNewCardModal(true)}
-                  />
-                }
-              />
-              {showNewCardModal && (
-                <ModalNewCard
-                  setShowNewCardModal={setShowNewCardModal}
-                  accounts={accounts}
-                />
-              )}
+  return (
 
     isLoaded ?
       <section className="grid lg:grid-cols-2 overflow-hidden p-3 pt-16 gap-4">
@@ -131,14 +85,12 @@ const AccountContent = () => {
 
               </article>
               <article className="grid gap-10">
-                <h4 className="font-medium">Lorem ipsum, dolor sit amet consectetur</h4>
+                <h4 className="font-bold text-white">Bienvenido <span className="bg-gradient-to-r from-yellow to-orange text-transparent bg-clip-text">
+                 
+                </span>{' '} a nuestra plataforma bancaria segura y confiable</h4>
                 <div className="grid sm:grid-cols-2">
                   <p className="text-left">
-                    {data.name}
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quos
-                    ab dolorem eum dolore cum, non distinctio eligendi obcaecati ipsum
-                    quas cupiditate aspernatur, eveniet quo consectetur iure officia
-                    earum quisquam.
+                    Donde podrás acceder fácilmente a la información de tus cuentas financieras. Aquí, te brindamos una visión completa y transparente de tus activos y pasivos, poniendo a tu alcance un panorama claro de tus finanzas personales.
                   </p>
                   <div>
                     <img
@@ -149,36 +101,25 @@ const AccountContent = () => {
                     />
                   </div>
                 </div>
-            </article>
+              </article>
+            </div>
+            <UltimosMovimientos account={accounts[selectedIndex]} />
+          </>
+          :
+          <div className="col-span-2 flex flex-col gap-10 items-center">
+            <h2>No se han encontrado cuentas</h2>
+            <div className="w-[150px]">
+              <Accesos text={"Solicitar nueva cuenta"} icon={<RiShakeHandsFill onClick={() => setShowNewAccountModal(true)} />} />
+              {showNewAccountModal && <ModalNewAccount setShowNewAccountModal={setShowNewAccountModal} accounts={accounts} />}
+            </div>
           </div>
-          <UltimosMovimientos account={accounts[selectedIndex]} />
-        </>
-      ) : (
-        <div className="col-span-2 flex flex-col gap-10 items-center">
-          <h2>No se han encontrado cuentas</h2>
-          <div className="w-[150px]">
-            <Accesos
-              text={"Solicitar nueva cuenta"}
-              icon={
-                <RiShakeHandsFill
-                  onClick={() => setShowNewAccountModal(true)}
-                />
-              }
-            />
-            {showNewAccountModal && (
-              <ModalNewAccount
-                setShowNewAccountModal={setShowNewAccountModal}
-                accounts={accounts}
-              />
-            )}
-          </div>
-        </div>
-      )}
-    </section>
-  ) : (
-    <div className="flex flex-row justify-center items-center h-screen">
-      <GridLoader color="white" />
-    </div>
-  );
-};
-export default AccountContent;
+
+        }
+      </section>
+      : <div className="flex flex-row justify-center items-center h-screen">
+        <GridLoader color="white" />
+      </div>
+
+  )
+}
+export default AccountContent
