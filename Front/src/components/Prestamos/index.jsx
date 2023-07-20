63f68908cb7699cc119d316856e5f9b9123a2757
FabricioReyna <fabricioreynacordes@gmail.com>
2023-07-20 19:03:42 -0300

    Prestamos V.1.2

---
+++ Front/src/components/Prestamos/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Loans = () => {
+    return(
+        <main>
+        <h2>Prestamos</h2>
+        </main>
+    )
+}
+
+export default Loans
---
+++ Front/src/pages/Prestamos/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Loans from "../../components/Prestamos";
+
+const LoanPri = () => {
+    return (
+        <section className="bg-gray rounded-xl min-h-screen w-screen">
+            <Loans/>
+        </section>
+    )
+
+}
+export default LoanPri
